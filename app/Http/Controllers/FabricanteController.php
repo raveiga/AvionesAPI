3704e2085371d79c12ee4b70639078f2a00b231c
Rafa Veiga <profesorveiga@gmail.com>
2015-04-15 21:59:47 +0200

    Modificación de Controlador Fabricante y desactivacion CSRF.
    - Creación método store Fabricantes
    - Creación método DELETE Fabricantes
    - Desactivamos control CSRF en Laravel.

--- app/Http/Controllers/FabricanteController.php
+++ app/Http/Controllers/FabricanteController.php
@@ -8,6 +8,8 @@ use Illuminate\Http\Request;
 // Cargamos Fabricante por que lo usamos más abajo.
 use App\Fabricante;
 
+use Response;
+
 class FabricanteController extends Controller {
 
 	/**
@@ -44,9 +46,22 @@ class FabricanteController extends Controller {
 	 *
 	 * @return Response
 	 */
-	public function store()
+	public function store(Request $request)
 	{
-		//
+		// Método llamado al hacer un POST.
+		// Comprobamos que recibimos todos los campos.
+		if (!$request->input('nombre') || !$request->input('direccion') || !$request->input('telefono'))
+		{
+			// NO estamos recibiendo los campos necesarios. Devolvemos error.
+			return response()->json(['errors'=>array(['code'=>422,'message'=>'Faltan datos necesarios para procesar el alta.'])],422);
+		}
+
+		// Insertamos los datos recibidos en la tabla.
+		$nuevoFabricante=Fabricante::create($request->all());
+
+		// Devolvemos la respuesta Http 201 (Created) + los datos del nuevo fabricante + una cabecera de Location + cabecera JSON
+		$respuesta= Response::make(json_encode(['data'=>$nuevoFabricante]),201)->header('Location','http://www.dominio.local/fabricantes/'.$nuevoFabricante->id)->header('Content-Type','application/json');
+		return $respuesta;
 	}
 
 	/**
@@ -57,7 +72,20 @@ class FabricanteController extends Controller {
 	 */
 	public function show($id)
 	{
-		//
+		// Corresponde con la ruta /fabricantes/{fabricante}
+		// Buscamos un fabricante por el ID.
+		$fabricante=Fabricante::find($id);
+
+		// Chequeamos si encontró o no el fabricante
+		if (! $fabricante)
+		{
+			// Se devuelve un array errors con los errores detectados y código 404
+			return response()->json(['errors'=>Array(['code'=>404,'message'=>'No se encuentra un fabricante con ese código.'])],404);
+		}
+
+		// Devolvemos la información encontrada.
+		return response()->json(['status'=>'ok','data'=>$fabricante],200);
+
 	}
 
 	/**
@@ -90,7 +118,29 @@ class FabricanteController extends Controller {
 	 */
 	public function destroy($id)
 	{
-		//
+		// Borrado de un fabricante.
+		// Ejemplo: /fabricantes/89 por DELETE
+		// Comprobamos si el fabricante existe o no.
+		$fabricante=Fabricante::find($id);
+
+		if (! $fabricante)
+		{
+			// Devolvemos error codigo http 404
+			return response()->json(['errors'=>array(['code'=>404,'message'=>'No se encuentra el fabricante con ese código.'])],404);
+		}
+
+		// Borramos el fabricante y devolvemos código 204
+		// 204 significa "No Content".
+		// Este código no muestra texto en el body.
+		// Si quisiéramos ver el mensaje devolveríamos
+		// un código 200.
+
+		$fabricante->delete();
+
+		return response()->json(['code'=>204,'message'=>'Se ha eliminado correctamente el fabricante.'],204);
+
+
+
 	}
 
 }
