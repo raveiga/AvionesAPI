4e4a7ae12322a8b6004d4dc7f441229d74218926
Rafa Veiga <profesorveiga@gmail.com>
2015-04-21 22:09:17 +0200

    Versionado de la API
    - Modificado routes.php para añadir versión 1.0 de la api : api/v1.0

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,27 +11,39 @@
 |
 */
 
-// Creamos las rutas nuevas que tendrán en cuenta
-// los controllers programados en Controllers.
+// Versionado de la API.
+// Las rutas quedarán algo como /api/v1.0/rutas existentes...
+// /api/v1.0/fabricantes/....
 
-// Ruta /fabricantes/.....
-Route::resource('fabricantes','FabricanteController',['except'=>['edit','create']]);
+Route::group(array('prefix'=>'api/v1.0'),function()
+{
+	// Creamos las rutas nuevas que tendrán en cuenta
+	// los controllers programados en Controllers.
 
-// Recurso anidado /fabricantes/xx/aviones/xxx
-Route::resource('fabricantes.aviones','FabricanteAvionController',['except'=>['edit','create','show']]);
+	// Ruta /fabricantes/.....
+	Route::resource('fabricantes','FabricanteController',['except'=>['edit','create']]);
 
-// Ruta /aviones/..... El resto de métodos los gestiona FabricanteAvion
-Route::resource('aviones','AvionController',['only'=>['index','show']]);
+	// Recurso anidado /fabricantes/xx/aviones/xxx
+	Route::resource('fabricantes.aviones','FabricanteAvionController',['except'=>['edit','create','show']]);
+
+	// Ruta /aviones/..... El resto de métodos los gestiona FabricanteAvion
+	Route::resource('aviones','AvionController',['only'=>['index','show']]);
 
 
-// Ruta por defecto /
-Route::get('/', function()
+	// Ruta por defecto al entrar http://www.dominio.local/api/v1.0
+	Route::get('/', function()
 	{
 		return "Bienvenido API RESTful de Aviones.";
 	});
 
+}); // Fin versionado
 
 
+// Ruta por defecto al entrar en http://www.dominio.local
+Route::get('/', function()
+{
+	return "<a href='http://www.dominio.local/api/v1.0'>Por favor acceda a la version 1.0 de la API.</a>";
+});
 /*
 Route::get('/', 'WelcomeController@index');
 
